19fcabb63df97285475908dac27802bc2c0dc364
christopher <coronac511@gmail.com>
2021-03-28 20:38:46 -0400

    added JS

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const top =  document.getElementsByTagName('h1')[0]
-top.innerText = "Christopher Corona"
+const title =  document.getElementsByClassName("back")[0]
+const backtitle = document.getElementsByTagName("h1")[0]
 
+backtitle.innerText = "Christopher Corona"
+
+const string1 =  document.getElementsByTagName("h2")[0]
+string1.innerText = "About me."
+
+const string2 =  document.getElementsByTagName("h2")[1]
+string2.innerText = "what I’m doing now."
+
+const string3 =  document.getElementsByTagName("h2")[2]
+string3.innerText = "why i started software development."
+
+const string4 =  document.getElementsByTagName("h2")[3]
+string4.innerText = "Languajes"
